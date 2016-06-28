753c4a91e3bb745e40157805b2047867ae749686
root <root@vlmihnevich2.example.com>
2016-06-28 12:32:05 -0400

    Updated STATIC and MEDIA dirs.

--- blast/blast/settings.py
+++ blast/blast/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+PARENT_DIR = os.path.dirname(BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
@@ -166,8 +166,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PARENT_DIR, 'static/')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(PARENT_DIR, 'media/')
 
 AUTH_USER_MODEL = 'users.User'
