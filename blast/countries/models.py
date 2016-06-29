6beaffd3250f5cd241eba9a6f0c5fee00310190b
MihnevichVL <vlmihnevoci@gmail.com>
2016-06-29 18:34:06 +0700

    Increase max_length for country code.

--- blast/countries/models.py
+++ blast/countries/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Country(models.Model):
     name = models.CharField(max_length=50, unique=True)
-    code = models.CharField(max_length=6)
+    code = models.CharField(max_length=10)
 
     def __str__(self):
         return '{}'.format(self.name)
