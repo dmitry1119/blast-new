a198699dc923db648506992c944eda708e408773
MihnevichVL <vlmihnevoci@gmail.com>
2016-07-14 23:32:08 +0700

    Add is_actual field to admin.

--- blast/smsconfirmation/admin.py
+++ blast/smsconfirmation/admin.py
@@ -6,7 +6,8 @@ from smsconfirmation.models import PhoneConfirmation
 @admin.register(PhoneConfirmation)
 class PhoneConfirmationAdmin(admin.ModelAdmin):
     list_display = ('phone', 'code', 'created_at', 'updated_at',
-                    'is_delivered', 'is_confirmed')
+                    'is_delivered', 'is_confirmed', 'is_actual')
+    list_editable = ('is_confirmed', 'is_delivered',)
 
     class Meta:
         model = PhoneConfirmation
