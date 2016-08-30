7e6f9b981843ebe46d18dbaabc2e0f0bf153e2e7
MihnevichVL <vlmihnevoci@gmail.com>
2016-08-30 17:09:11 +0700

    Add push notification action to notifications admin.

--- blast/notifications/admin.py
+++ blast/notifications/admin.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
 from notifications.models import Notification, FollowRequest
 
+from notifications.tasks import send_push_notification
+
+
+def resend(modeladmin, request, qs):
+    for it in qs:
+        send_push_notification.delay(it.user_id, it.text, it.push_payload)
+
+resend.short_description = 'Send push notifications'
 
 @admin.register(Notification)
 class NotificationAdmin(admin.ModelAdmin):
     list_display = ('id', 'user', 'text', 'created_at')
+    actions = [resend]
 
 
 @admin.register(FollowRequest)
