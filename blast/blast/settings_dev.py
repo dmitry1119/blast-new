3e87f883e43d788da5032aeb922e7906af2d46c0
vlmihnevich <vlmihnevich@gmail.com>
2016-10-11 22:08:21 +0700

    Improved config for dev configuration.

--- blast/blast/settings_dev.py
+++ blast/blast/settings_dev.py
@@ -4,6 +4,17 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
+CELERYBEAT_SCHEDULE = {
+    'clear-expired-posts': {
+        'task': 'posts.tasks.clear_expired_posts',
+        'schedule': timedelta(seconds=60*5),
+    },
+    'send-notifications': {
+        'task': 'posts.tasks.send_expire_notifications',
+        'schedule': timedelta(seconds=60*3)
+    }
+}
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
