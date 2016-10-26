46b7eea844894a99c81b375520c549de2b646f68
vlmihnevich <vlmihnevich@gmail.com>
2016-10-26 21:22:53 +0700

    Add memoize_list decorator.

--- blast/core/decorators.py
+++ blast/core/decorators.py
@@ -12,7 +12,7 @@ def save_to_zset(key_pattern: str):
         def wrapped_function(pk, start: int, end: int):
             key = key_pattern.format(pk)
             if not r.exists(key):
-                logger.info('Head up cache for {}'.format(key))
+                logger.info('Heat up cache for {}'.format(key))
                 result = f(pk, start, end)
 
                 if not result:
@@ -26,5 +26,26 @@ def save_to_zset(key_pattern: str):
                 r.zadd(key, *result)
             result = r.zrevrange(key, start, end)
             return [int(it) for it in result]
+        return wrapped_function
+    return wrap
+
+
+def memoize_list(key_pattern: str):
+    def wrap(f):
+        def wrapped_function(pk: int, start: int, end: int):
+            key = key_pattern.format(pk)
+            if not r.exists(key):
+                logger.info('Heat up cache for %s', key)
+                result = f(pk, start, end)
+
+                if not result:
+                    logger.info('Nothing to cache for %s key', key)
+                    return []
+
+                for it in result:
+                    r.lpush(key, it)
+
+            cached = r.lrange(key, start, end)
+            return (int(it) for it in cached)
         return wrapped_function
     return wrap
